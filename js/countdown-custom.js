0e955fffa73478556a875df1024b925a742870b5
Govind Kumar <govind.kumar@i.suprdaily.com>
2022-04-06 02:41:17 +0530

    updated landing page

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 4, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022, 3, 9, 8)}); // year, month, date, hour
         });
 });		
 
